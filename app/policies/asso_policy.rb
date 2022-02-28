66673d926f22507e9e520d8b6aa9c1a9bd474fa8
Julie Gastellu <julie.gastellu@orange.fr>
2022-02-28 16:41:50 +0100

    pundit

---
+++ app/policies/asso_policy.rb
@@ -0,0 +1,8 @@
+class AssoPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+end
