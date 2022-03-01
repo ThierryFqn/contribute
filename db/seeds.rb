2bbde19f79753df88e121e39f604a6710d27b93f
Thierry Fonquernie <thierry.fonquernie@gmail.com>
2022-03-01 16:44:12 +0100

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
 require "open-uri"
 require "nokogiri"
+require "faker"
 
 puts 'Destroy All Users'
 User.destroy_all
