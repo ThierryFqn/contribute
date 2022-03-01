7b78c65ec3613f861f56b8261c24b1e469d9229d
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-03-01 16:01:27 +0100

    add profile

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,7 @@
+class ProfilesController < ApplicationController
+  def show
+    authorize(:profile, :show?)
+    @participations = current_user.participations
+    @events = @participations.map(&:event)
+  end
+end
