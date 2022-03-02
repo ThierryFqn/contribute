0a5d62e38d9f9c5a8ddd4e5e5c1f719d30e134ae
Julie Gastellu <julie.gastellu@orange.fr>
2022-03-02 11:18:35 +0100

    log-Front

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,26 @@ class EventsController < ApplicationController
 
   def index
     @events = policy_scope(Event).order(created_at: :desc)
+    @events = policy_scope(Event).order(created_at: :desc)
+
+    if params[:search].present?
+      if search_params[:start_date, :end_date].reject(&:empty?).present? && search_params[:address].present?
+        @events = policy_scope(Event).order(created_at: :desc)
+                                         .where(personality: search_params[:start_date, :end_date].reject(&:empty?))
+                                         .and(policy_scope(Event).where("address ILIKE ?", "%#{search_params[:address]}%"))
+
+      elsif search_params[:start_date, :end_date].reject(&:empty?).present? && !search_params[:address].present?
+        @events = policy_scope(Event).order(created_at: :desc)
+                                         .where(personality: search_params[:start_date, :end_date].reject(&:empty?))
+
+      elsif !search_params[:start_date, :end_date].reject(&:empty?).present? && search_params[:address].present?
+        @events = policy_scope(Event).order(created_at: :desc)
+                                         .where("address ILIKE ?", "%#{search_params[:address]}%")
+      end
+
+    else
+      @events = policy_scope(Event).order(created_at: :desc)
+    end
   end
 
   def show
@@ -38,4 +58,9 @@ class EventsController < ApplicationController
   def params_event
     params.require(:event).permit(:name, :description, :cause, :status, :start_date, :end_date, :address, :number_volunteers)
   end
+
+  def search_params
+    params.require(:search).permit(:address, :start_date, :end_date)
+  end
+
 end
