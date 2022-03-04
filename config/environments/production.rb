8e64b2abbabeca66bce072d3b6c5ebe6a20541b3
Thierry Fonquernie <thierry.fonquernie@gmail.com>
2022-03-04 11:04:29 +0100

    send in blue for prod srtup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,4 +118,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp-relay.sendinblue.com',
+    :port => 587,
+    :user_name => ENV['SENDINBLUE_EMAIL'],
+    :password => ENV['SENDINBLUE_PASSWORD'],
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
 end
