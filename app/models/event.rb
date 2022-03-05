1f838058be70b24f23ac68a00d91df650be675ef
Thierry Fonquernie <thierry.fonquernie@gmail.com>
2022-03-05 12:47:23 +0100

    participation_rate Done

--- app/models/event.rb
+++ app/models/event.rb
@@ -24,6 +24,11 @@ class Event < ApplicationRecord
 
   def attach_photo
     return if photos.attached?
+
     self.photos.attach(io: File.open(File.join(Rails.root,'app/assets/images/default-image.jpg')), filename: 'default image')
+  end
+
+  def hours_calcul
+    (end_date.to_time - start_date.to_time) / 1.hours
   end
 end
